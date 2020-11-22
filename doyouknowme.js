cf6823cd7b07944c07be03302ef290aa8ff266ba
Pragya <sabharwalpragya@gmail.com>
2020-11-22 16:54:54 +0530

    docs: questions

--- doyouknowme.js
+++ doyouknowme.js
@@ -1,7 +1,44 @@
 const chalk = require('chalk')
 const log = console.log
 
-console.log("Welcome. Let us play a quiz today\n")
+log(chalk.bgMagenta("Welcome. Let us play a quiz today\n"))
 
 var readlineSync = require("readline-sync")
-var userName = readlineSync.question("What is you name?")
+
+var response = readlineSync.question(chalk.bold.white.bgBlue("Are you ready? (Yes/No) "))
+console.log("\n")
+
+if (response.toUpperCase() === YES) {
+    var userName = readlineSync.question(chalk.bgMagenta("What is your name? "))
+    console.log("\n")
+    log(chalk.bgBlue("Hi " + userName + ". Let the game begin!"))
+    console.log("\n")
+
+    score = 0;
+
+    questions =[
+        {
+            question:"Where do I live? ",
+            answer: "Delhi"},
+            {
+            question:"What is my age? ",
+            answer: "20"},
+            {
+            question:"What is my favorite color? ",
+            answer: "Orange"
+          },{
+            question:"Do I like tea or coffee? ",
+            answer: "Coffee"
+          },{
+            question:"Which city is my college in? ",
+            answer:"Delhi"},
+            {
+            question:"Do I like to read? ",
+            answer:"Yes"
+          },{
+            question:"Movies/Books? ",
+            answer:"Books"},{
+            question:"Which is my birthday month? ",
+            answer:"November"}
+    ]
+
